710972e1554948d788961f6d2a2a15755dc2f487
Andrew <andrew.martin.louis@gmail.com>
2022-05-29 15:43:27 -0700

    Removed redundant objec t

--- server.lua
+++ server.lua
@@ -4,15 +4,6 @@ TriggerEvent("getCore",function(core)
     VorpCore = core
 end)
 
-IsTownBanned = function (town)
-	for k,v in pairs(Config.BannedTowns) do
-		if town == GetHashKey(v) then
-			return true
-		end
-	end
-	return false
-end
-
 VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 
 Citizen.CreateThread(function()
