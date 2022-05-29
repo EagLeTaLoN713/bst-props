a6bdac52ddab50d24027270d9b04b82e48a21152
EagLeTaLoN713 <104523142+EagLeTaLoN713@users.noreply.github.com>
2022-05-29 23:23:09 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,2 @@
+Config = {}
+Config.BannedTowns = {'Annesburg', 'Armadillo', 'Blackwater', 'BeechersHope', 'Breaithwaite', 'Butcher', 'Caliga', 'Cornwall', 'Emerald', 'Lagras', 'Manzanita', 'Rhodes', 'Siska', 'StDenis', 'Strawberry', 'Tumbleweed', 'Valentine', 'Vanhorn', 'Wallace', 'Wapiti', 'AguasducesFarm', 'AguasdulcesRuins', 'AguasdulcesVilla', 'Manico'}
