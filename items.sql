83c8d3409ab556146f3c2d27a361e47cb3390ae3
EagLeTaLoN713 <104523142+EagLeTaLoN713@users.noreply.github.com>
2022-06-07 13:28:41 +0100

    Update items.sql

--- items.sql
+++ items.sql
@@ -1,1 +1,9 @@
 
+USE `vorp`;
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tent', 'Tent', 1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tentlarge', 'Large Tent', 1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('flint', 'Flint', 1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('deadwood', 'Kindling', 1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('campfire', 'Campfire', 1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('bedroll', 'BedRoll', 1, 1, 'item_standard', 0);
