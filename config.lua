f1fc96f94e5b6f4c516b6f065d525259f02ad994
EagLeTaLoN713 <104523142+EagLeTaLoN713@users.noreply.github.com>
2022-06-07 13:20:10 +0100

    Update config.lua
    added craftTime for better sync to animations/scenarios

--- config.lua
+++ config.lua
@@ -1,2 +1,5 @@
 Config = {}
+-- How long the progressbar will show when crafting
+Config.CraftTime = 15000
+
 Config.BannedTowns = {'Annesburg', 'Armadillo', 'Blackwater', 'BeechersHope', 'Breaithwaite', 'Butcher', 'Caliga', 'Cornwall', 'Emerald', 'Lagras', 'Manzanita', 'Rhodes', 'Siska', 'StDenis', 'Strawberry', 'Tumbleweed', 'Valentine', 'Vanhorn', 'Wallace', 'Wapiti', 'AguasducesFarm', 'AguasdulcesRuins', 'AguasdulcesVilla', 'Manico'}
